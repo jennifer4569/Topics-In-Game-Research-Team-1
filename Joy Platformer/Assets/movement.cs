205b9989f65c87804fb4b12fe684e6b0c3d17e7b
Jennifer Zhang <jennifer.newyork@aol.com>
2022-01-26 19:08:52 -0500

    added test files

--- Joy Platformer/Assets/movement.cs
+++ Joy Platformer/Assets/movement.cs
@@ -8,8 +8,8 @@ public class movement : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
+    private float playerSpeed = 10f;
+    private float jumpHeight = 150f;
     private float gravityValue = -9.81f;
     public Camera cam;
     private bool canMoveAgain = true;
@@ -42,34 +42,19 @@ public class movement : MonoBehaviour
               // do whatever you want
             }
           }
-          
+
         if(canMoveAgain == true){
 
-        groundedPlayer = controller.isGrounded;
-            if (groundedPlayer && playerVelocity.y < 0)
-            {
-                playerVelocity.y = 0f;
-            }
-    
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            controller.Move(move * Time.deltaTime * playerSpeed);
-    
-            if (move != Vector3.zero)
-            {
-                gameObject.transform.forward = move;
-            }
-    
-            // Changes the height position of the player..
+            groundedPlayer = controller.isGrounded;
+            Vector3 move = new Vector3(Input.GetAxis("Horizontal") * playerSpeed, 0, Input.GetAxis("Vertical") * playerSpeed);
+
+            //Changes the height position of the player..
             if (Input.GetButtonDown("Jump") && groundedPlayer)
             {
-                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+                move.y = jumpHeight;
             }
-    
-            playerVelocity.y += gravityValue * Time.deltaTime;
-            controller.Move(playerVelocity * Time.deltaTime);
-        }
-        }
-          
-        
-      
+            move.y = move.y + Physics.gravity.y * .5f;
+            controller.Move(move * Time.deltaTime);
+       }
     }
+}
